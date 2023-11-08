720d3248efb5d57d7749524f7d34904d0c9e5ca0
Joel Chamorro <kevin.j03@hotmail.com>
2023-11-08 13:43:04 -0500

    JCH - First commit

---
+++ src/main/java/com/pfc2/weather/api/entities/UserEntity.java
@@ -0,0 +1,64 @@
+package com.pfc2.weather.api.entities;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+@Builder
+@Getter
+@Setter
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/test/java/com/pfc2/weather/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfc2.weather.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
